908dad77d74eb309b7be2d81c3eb7a377b20b029
ahmadullahmukhlis <nasarimukhlis@gmail.com>
2024-12-15 19:22:53 +0430

    the problem of the tes laayout is fxed

--- src/layouts/default-layout/config/DefaultLayoutConfig.ts
+++ src/layouts/default-layout/config/DefaultLayoutConfig.ts
@@ -1,73 +1,74 @@
 import type LayoutConfigTypes from "@/layouts/default-layout/config/types";
 
-const config: LayoutConfigTypes = {
-  general: {
-    mode: "system",
-    primaryColor: "#50CD89",
-    pageWidth: "default",
-    layout: "dark-sidebar",
-    iconsType: "duotone",
-  },
-  
-  header: {
-    display: true,
-    default: {
-      container: "fluid",
-      fixed: {
-        desktop: true,
-        mobile: true,
-      },
-      menu: {
-        display: true,
-        iconType: "keenthemes",
-      },
+const config: LayoutConfigTypes = 
+  {
+    "general": {
+      "mode": "system",
+      "primaryColor": "#50CD89",
+      "pageWidth": "default",
+      "layout": "dark-sidebar",
+      "iconsType": "duotone"
     },
-  },
-  sidebar: {
-    display: true,
-    default: {
-      minimize: {
-        desktop: {
-          enabled: true,
-          default: false,
-          hoverable: true,
+    "header": {
+      "display": true,
+      "default": {
+        "container": "fluid",
+        "fixed": {
+          "desktop": true,
+          "mobile": true
         },
-      },
-      menu: {
-        iconType: "keenthemes",
-      },
+        "menu": {
+          "display": true,
+          "iconType": "keenthemes"
+        }
+      }
     },
-  },
-  toolbar: {
-    display: true,
-    container: "fluid",
-    fixed: {
-      desktop: false,
-      mobile: false,
+    "sidebar": {
+      "display": true,
+      "default": {
+        "minimize": {
+          "desktop": {
+            "enabled": true,
+            "default": false,
+            "hoverable": true
+          }
+        },
+        "menu": {
+          "iconType": "keenthemes"
+        }
+      }
     },
-  },
-  pageTitle: {
-    display: true,
-    breadcrumb: true,
-    direction: "column",
-  },
-  content: {
-    container: "fluid",
-  },
-  footer: {
-    display: true,
-    container: "fluid",
-    fixed: {
-      desktop: false,
-      mobile: false,
+    "toolbar": {
+      "display": true,
+      "container": "fluid",
+      "fixed": {
+        "desktop": false,
+        "mobile": false
+      }
     },
-  },
-  illustrations: {
-    set: "sketchy-1",
-  },
-  scrolltop: {
-    display: true,
-  },
-};
+    "pageTitle": {
+      "display": true,
+      "breadcrumb": true,
+      "direction": "column"
+    },
+    "content": {
+      "container": "fluid"
+    },
+    "footer": {
+      "display": true,
+      "container": "fluid",
+      "fixed": {
+        "desktop": false,
+        "mobile": false
+      }
+    },
+    "illustrations": {
+      "set": "sketchy-1"
+    },
+    "scrolltop": {
+      "display": true
+    }
+  }
+
 
 export default config;
